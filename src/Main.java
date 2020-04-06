16d354b2d0975963c1d103932542ccde58d2266a
jeremy.gau <caliban3010@gmail.com>
2020-04-06 17:52:39 +0200

    Fin partie 1

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,14 @@
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
-        Graph graph = new Graph(10, 0.5);
-        System.out.println(graph.getNeighbours());
+        Graph graph = new Graph(4, 0.5);
+        System.out.println(graph.getNeighbours() + "\n");
+        for (Integer summit : graph.summits) {
+//            System.out.println(graph.dijkstra(graph, summit));
+            System.out.println(Arrays.toString(graph.dijkstra(graph, summit)));
+            System.out.println();
+        }
     }
 }
